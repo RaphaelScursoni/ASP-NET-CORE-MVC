ff25c5d250294b850c9a23e24bc656155b1eef04
Raphael Scursoni <ra.scursoni@gmail.com>
2021-02-21 19:24:35 -0300

    Primeiro controller e teste das paginas Razor

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -13,7 +13,9 @@ namespace SalesWebMvc.Controllers {
         }
 
         public IActionResult About() {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Salles Web MVC App from C# study";
+            ViewData["Email"] = "ra.scursoni@gmail.com";
+            ViewData["Aluno"] = "Raphael Scursoni";
 
             return View();
         }
